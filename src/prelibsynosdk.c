bbe45e3c5da07855a37d97d076131a47e51f72af
jinlife <86603404+jinlife@users.noreply.github.com>
2022-04-28 14:02:28 +0800

    Update prelibsynosdk.c

--- src/prelibsynosdk.c
+++ src/prelibsynosdk.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool SYNOFSIsRemoteFS(int) {
+bool SYNOFSIsRemoteFS(int a1) {
     printf("_BOOL8 __fastcall SYNOFSIsRemoteFS(int a1) return false(0)\n");
     return false;
 }
