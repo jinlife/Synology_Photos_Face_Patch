89da5045d7c0a8d209b61984b73564f67f88cd59
jinlife <86603404+jinlife@users.noreply.github.com>
2022-09-19 14:48:13 +0800

    Update auto_patch_Photos.sh

--- lazy/auto_patch_Photos.sh
+++ lazy/auto_patch_Photos.sh
@@ -10,21 +10,12 @@ tar -C ./synoesp/ -xf oldpat.tar.gz rd.gz
 cd synoesp
 
 output=$(xz -dc < rd.gz 2>/dev/null | cpio -idm 2>&1)
-mkdir extract && cd extract
-cp -v ../usr/lib/libcurl.so.4 ../usr/lib/libmbedcrypto.so.5 ../usr/lib/libmbedtls.so.13 ../usr/lib/libmbedx509.so.1 ../usr/lib/libmsgpackc.so.2 ../usr/lib/libsodium.so ../usr/lib/libsynocodesign-ng-virtual-junior-wins.so.7 ../usr/syno/bin/scemd ./
-ln -s scemd syno_extract_system_patch
-
-cd ..
 #ls
-#echo "start to pack again"
-echo "Start to patch libsynosdk.so.7"
-mkdir hda1Extract
-tar -C hda1Extract -xf hda1.tgz
-echo "hda1 in"
+echo "Start to extract synoarchive"
+mkdir hda1Extract && cd hda1Extract
 #ls
-echo "cp file libsynosdk.so.7"
-cp -v ./hda1Extract/usr/lib/libsynosdk.so.7 ../../../
-cp -v ./hda1Extract/usr/syno/sbin/synoarchive ./
+echo "cp file synoarchive"
+cp -v ../usr/syno/sbin/synoarchive ./
 ln -s synoarchive syno_extract_spk
 
 echo "Start patch Photos"
@@ -33,16 +24,16 @@ echo ${photo_address}
 #https://global.download.synology.com/download/Package/spk/SynologyPhotos/1.2.0-0263/SynologyPhotos-x86_64-1.2.0-0263.spk
 curl --location  ${photo_address} --output ${photo_version}.spk
 mkdir output-spk
-sudo LD_LIBRARY_PATH=./hda1Extract/usr/lib/ ./syno_extract_spk -vxf ${photo_version}.spk -C output-spk
+sudo LD_LIBRARY_PATH=../usr/lib/ ./syno_extract_spk -vxf ${photo_version}.spk -C output-spk
 cd output-spk
 #ls
 echo "Extract packages"
 mkdir package
 tar -C package -xf package.tgz
 #ls package
-cp -v ./package/usr/lib/libsynophoto-plugin-model.so ../../../../
+cp -v ./package/usr/lib/libsynophoto-plugin-model.so ../../../../../
 
-cd ../../../../
+cd ../../../../../
 
 ../bin/PatchELFSharp-Linux64/PatchELFSharp "libsynophoto-plugin-model.so" "_ZN9synophoto6plugin7network9IeNetwork11IsSupportedEv" "B8 00 00 00 00 C3"
 cp -v libsynophoto-plugin-model.so ../libsynophoto-plugin-model.so
