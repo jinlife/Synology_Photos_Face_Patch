42ef426cc19e4fba3699bbfe5f9524ac682f744f
jinlife <glucose1e@tom.com>
2024-09-28 10:46:21 +0800

    Use synology directly.

--- lazy/auto_patch_Photos.sh
+++ lazy/auto_patch_Photos.sh
@@ -4,22 +4,22 @@
 photo_version=$1
 
 # download old pat for syno_extract_system_patch # thanks for jumkey's idea.
-mkdir synoesp
-curl --location https://global.download.synology.com/download/DSM/release/7.0.1/42218/DSM_DS3622xs%2B_42218.pat --output oldpat.tar.gz
-tar -C ./synoesp/ -xf oldpat.tar.gz hda1.tgz
-cd synoesp
-
-output=$(xz -dc < hda1.tgz 2>/dev/null | cpio -idm 2>&1)
-
-#echo "show usr syno"
-#ls ./usr/syno/
-echo "Start to extract synoarchive"
-#ls ./usr/syno/sbin/
-mkdir extract && cd extract
-#ls
-echo "cp file synoarchive"
-cp -v ../usr/syno/sbin/synoarchive ./
-ln -s synoarchive syno_extract_spk
+# mkdir synoesp
+# curl --location https://global.download.synology.com/download/DSM/release/7.0.1/42218/DSM_DS3622xs%2B_42218.pat --output oldpat.tar.gz
+# tar -C ./synoesp/ -xf oldpat.tar.gz hda1.tgz
+# cd synoesp
+# 
+# output=$(xz -dc < hda1.tgz 2>/dev/null | cpio -idm 2>&1)
+# 
+# #echo "show usr syno"
+# #ls ./usr/syno/
+# echo "Start to extract synoarchive"
+# #ls ./usr/syno/sbin/
+# mkdir extract && cd extract
+# #ls
+# echo "cp file synoarchive"
+# cp -v ../usr/syno/sbin/synoarchive ./
+# ln -s synoarchive syno_extract_spk
 
 echo "Start patch Photos"
 photo_address="https://global.download.synology.com/download/Package/spk/SynologyPhotos/${photo_version}/SynologyPhotos-x86_64-${photo_version}.spk"
@@ -27,7 +27,8 @@ echo ${photo_address}
 #https://global.download.synology.com/download/Package/spk/SynologyPhotos/1.2.0-0263/SynologyPhotos-x86_64-1.2.0-0263.spk
 curl --location  ${photo_address} --output ${photo_version}.spk
 mkdir output-spk
-sudo LD_LIBRARY_PATH=../usr/lib/ ./syno_extract_spk -vxf ${photo_version}.spk -C output-spk
+#sudo LD_LIBRARY_PATH=../usr/lib/ ./syno_extract_spk -vxf ${photo_version}.spk -C output-spk
+sudo synoarchive -vxf ${photo_version}.spk -C output-spk
 cd output-spk
 #ls
 echo "Extract packages"
--- lazy/auto_patch_SynoSDK.sh
+++ lazy/auto_patch_SynoSDK.sh
@@ -5,16 +5,16 @@ os_main_version=$1
 # 42661
 os_version=$2
 
-# download old pat for syno_extract_system_patch # thanks for jumkey's idea.
-mkdir synoesp
-curl --location https://global.download.synology.com/download/DSM/release/7.0.1/42218/DSM_DS3622xs%2B_42218.pat --output oldpat.tar.gz
-tar -C ./synoesp/ -xf oldpat.tar.gz rd.gz
-cd synoesp
-
-output=$(xz -dc < rd.gz 2>/dev/null | cpio -idm 2>&1)
-mkdir extract && cd extract
-cp -v ../usr/lib/libcurl.so.4 ../usr/lib/libmbedcrypto.so.5 ../usr/lib/libmbedtls.so.13 ../usr/lib/libmbedx509.so.1 ../usr/lib/libmsgpackc.so.2 ../usr/lib/libsodium.so ../usr/lib/libsynocodesign-ng-virtual-junior-wins.so.7 ../usr/syno/bin/scemd ./
-ln -s scemd syno_extract_system_patch
+## download old pat for syno_extract_system_patch # thanks for jumkey's idea.
+#mkdir synoesp
+#curl --location https://global.download.synology.com/download/DSM/release/7.0.1/42218/DSM_DS3622xs%2B_42218.pat --output oldpat.tar.gz
+#tar -C ./synoesp/ -xf oldpat.tar.gz rd.gz
+#cd synoesp
+#
+#output=$(xz -dc < rd.gz 2>/dev/null | cpio -idm 2>&1)
+#mkdir extract && cd extract
+#cp -v ../usr/lib/libcurl.so.4 ../usr/lib/libmbedcrypto.so.5 ../usr/lib/libmbedtls.so.13 ../usr/lib/libmbedx509.so.1 ../usr/lib/libmsgpackc.so.2 ../usr/lib/libsodium.so ../usr/lib/libsynocodesign-ng-virtual-junior-wins.so.7 ../usr/syno/bin/scemd ./
+#ln -s scemd syno_extract_system_patch
 
 # Extract pat file
 #https://global.download.synology.com/download/DSM/release/7.1/42661/DSM_DS920+_42661.pat
@@ -42,8 +42,8 @@ else
 	fi
 fi
 
-sudo LD_LIBRARY_PATH=. ./syno_extract_system_patch ${os_version}.pat output-pat
-
+#sudo LD_LIBRARY_PATH=. ./syno_extract_system_patch ${os_version}.pat output-pat
+sudo scemd ${os_version}.pat output-pat
 cd output-pat
 #ls
 #echo "start to pack again"
